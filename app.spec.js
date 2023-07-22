48526ebbf51daced917926362aef6cee0a967a47
fddemarco <franco.demarco3400@gmail.com>
2023-07-22 12:20:19 -0300

    feat: Multiplication operation

--- app.spec.js
+++ app.spec.js
@@ -7,4 +7,7 @@ describe('Calculator operations', () => {
   test('Subtraction operation', () => {
     expect(new Calculator('5', '5', '-').calc()).toEqual(0);
   });
+  test('Multiplication operation', () => {
+    expect(new Calculator('5', '5', '*').calc()).toEqual(25);
+  });
 });
